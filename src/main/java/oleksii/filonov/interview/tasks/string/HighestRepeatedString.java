7fc312cc5f47fb3283f856fbd463556fac4f5702
Oleksii Filonov <oleksii.filonov@gmail.com>
2021-12-08 22:13:08 -0800

    Adding comments

--- src/main/java/oleksii/filonov/interview/tasks/string/HighestRepeatedString.java
+++ src/main/java/oleksii/filonov/interview/tasks/string/HighestRepeatedString.java
@@ -1,16 +1,18 @@
-package oleksii.filonov.interview;
+package oleksii.filonov.interview.tasks.string;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Return first String with the maximum number of repeated characters
+ */
 public class HighestRepeatedString {
-
 
     public static void main(String[] args) {
         HighestRepeatedString highestRepeatedString = new HighestRepeatedString();
-        System.out.println("1:'" + highestRepeatedString.highest_repeated("It is really simple and accurate!") + "'- end");
-        System.out.println("2:'" + highestRepeatedString.highest_repeated("It is really simple and accurate! ccc") + "'- end");
-        System.out.println("3:'" + highestRepeatedString.highest_repeated("a b c d") + "'- end");
+        System.out.println("1:'" + highestRepeatedString.highest_repeated("It is really simple and accurate!") + "'- should return 'really', because it is first with 2 characters");
+        System.out.println("2:'" + highestRepeatedString.highest_repeated("It is really simple and accurate! ccc") + "'- should return 'ccc', because it has max numbers of repeating characters");
+        System.out.println("3:'" + highestRepeatedString.highest_repeated("a b c d") + "'- should return nothing, there is no word with repeating characters in the word");
     }
 
     //O(n) - time complexity, n - number of characters in the initial String
